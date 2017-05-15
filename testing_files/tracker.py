45ced73b49f283690b67553543f525961a47764c
Max Oliver Geislinger <omnipotent@Maxs-MacBook-Air.local>
2017-05-15 07:48:30 -0700

    commit before merge

--- testing_files/tracker.py
+++ testing_files/tracker.py
@@ -2,6 +2,8 @@ import cv2
 import sys
 import dlib
 
+# from FaceRecognitionModel import FaceRecognitionModel
+
 def rect_to_bb(rect):
     # take a bounding predicted by dlib and convert it
     # to the format (x, y, w, h) as we would normally do
@@ -14,15 +16,23 @@ def rect_to_bb(rect):
     # return a tuple of (x, y, w, h)
     return (x, y, w, h)
 if __name__ == '__main__' :
- 
+    # Read video
+    video = cv2.VideoCapture("dance_HD.mp4")
+
+    # fm = FaceRecognitionModel()
+    # Find faces in image
+    # _, frame = video.read()
+    # cv2.imwrite("dance_frame.jpg",frame)
+    # face = fm.recognize("dance_frame.jpg")
+    # print face
+     
     # Set up tracker.
     # Instead of MIL, you can also use
     # BOOSTING, KCF, TLD, MEDIANFLOW or GOTURN
     print cv2.__version__ 
     tracker = cv2.Tracker_create("MEDIANFLOW") 
- 
-    # Read video
-    video = cv2.VideoCapture("out.mp4")
+    # tracker = cv2.Tracker_create("KCF") 
+
  
     # Exit if video not opened.
     if not video.isOpened():
@@ -37,7 +47,10 @@ if __name__ == '__main__' :
      
     # Define an initial bounding box
     # rectangle(124,95,214,185)
-    bbox = rect_to_bb(dlib.rectangle(124,95,214,185))
+    # dlib.rectangle(124,95,214,185) # this is for out.mp4 whiplash
+    # 
+    bbox = rect_to_bb(dlib.rectangle(780,130,832,181)) # will
+    # bbox = rect_to_bb(dlib.rectangle(509,164,561,216)) # Jimmy
 
     # rect.top(), rect.right(), rect.bottom(), rect.left()
 
@@ -46,8 +59,10 @@ if __name__ == '__main__' :
  
     # Initialize tracker with first frame and bounding box
     ok = tracker.init(frame, bbox)
+
+
     fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,360))
+    out = cv2.VideoWriter('output_medianflow_will.avi', fourcc, 20.0, (1280,720))# (640,360))
     while True:
         # Read a new frame
         ok, frame = video.read()
@@ -64,7 +79,7 @@ if __name__ == '__main__' :
             cv2.rectangle(frame, p1, p2, (0,0,255))
 
         # Display result
-        cv2.imshow("Tracking", frame)
+        # cv2.imshow("Tracking", frame)
         out.write(frame)
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
